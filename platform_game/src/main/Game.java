715f91ee7916df5eb1b4ec93912bf63e4ce14d1c
Nam Anh Tran <steve200994@gmail.com>
2026-01-19 15:23:58 +1100

    Implement game loop and input handling in Game class; update GamePanel for rendering

--- platform_game/src/main/Game.java
+++ platform_game/src/main/Game.java
@@ -1,10 +1,12 @@
 package main;
 
 //Everything relevant to game class
-public class Game {
+public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
-   
+    private Thread gameThread;
+    private final int FPS_SET = 120;
+
     //constructor - head method of the class, use to make object
     public Game(){
         gamePanel = new GamePanel(); //create game panel object
@@ -12,7 +14,33 @@ public class Game {
         gameWindow = new GameWindow(gamePanel);
         //request that this component get the input focus
         gamePanel.requestFocusInWindow();
-
-        
+        startGameLoop();
+    }
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        //nano-second -> duration each frame should last
+        double timePerFrame = 1000000000.0 / FPS_SET; 
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        int frames =0;
+        long lastCheck = System.currentTimeMillis();
+        //game loop
+        while (true){
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame){
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+            if(System.currentTimeMillis() - lastCheck >= 1000){
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames=0;
+            }
+        }
     }
 }
--- platform_game/src/main/GamePanel.java
+++ platform_game/src/main/GamePanel.java
@@ -5,16 +5,25 @@ import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 //make stuff appear on GameWindow
 public class GamePanel extends JPanel {
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private float xDelta = 100;
+    private float yDelta = 100;
+
+    // private float xDir = 1f;
+    // private float yDir =1f;
 
     private MouseInputs mouseInputs;
 
+    // private Random random;
+
+    private Color color = new Color(150,20,90);
+
     public GamePanel(){
+        // random = new Random();
         mouseInputs = new MouseInputs(this); //this here basically means when something happens, call methods on this GamePanel instance
         setFocusable(true);
         addKeyListener(new KeyboardInputs(this));
@@ -25,17 +34,14 @@ public class GamePanel extends JPanel {
     //
     public void changeXDelta (int value) {
         this.xDelta += value;
-        repaint(); //repaint the new changes
     }
     public void changeYDelta (int value) {
         this.yDelta += value;
-        repaint();
     }
 
     public void setRecPos(int x, int y){
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     // this is a method in JPanel, it is call when we press a play button.
@@ -43,6 +49,30 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g){
         //calling the super class in Jpanel
         super.paintComponent(g);
-        g.fillRect(xDelta, yDelta, 200, 50);
+        // updateRectangle();
+        g.setColor(color);
+        g.fillRect((int)xDelta, (int)yDelta, 200, 50);
     }
+
+    // private void updateRectangle(){
+   
+    //     if(xDelta > 400 || xDelta < 0){
+    //         xDir *= -1;
+    //         color = getRandomColor();
+    //     }
+   
+    //     if(yDelta > 400 || yDelta <0){
+    //         yDir *= -1;
+    //         color = getRandomColor();
+    //     }
+
+    // }
+
+    // private Color getRandomColor() {
+    //     int r = random.nextInt(255);
+    //     int g = random.nextInt(50);
+    //     int b = random.nextInt(100);
+
+    //     return new Color(r,g,b);
+    // }
 }
